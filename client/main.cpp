275dde7e0e555fec4f35e9ab1b4d4fa14de3266f
Hjalte Sorgenfrei Mac Dalland <hjaltesorgenfrei@gmail.com>
2023-04-02 00:59:32 +0200

    Use UDP instead

--- client/main.cpp
+++ client/main.cpp
@@ -1,91 +1,56 @@
 //
-// async_tcp_echo_server.cpp
-// ~~~~~~~~~~~~~~~~~~~~~~~~~
+// blocking_udp_echo_client.cpp
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //
-// Copyright (c) 2003-2020 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+// Copyright (c) 2003-2021 Christopher M. Kohlhoff (chris at kohlhoff dot com)
 //
-// Distributed under the Boost Software License, Version 1.0.
-// (See http://www.boost.org/LICENSE_1_0.txt)
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
 #include <cstdlib>
+#include <cstring>
 #include <iostream>
-#include <memory>
-#include <utility>
+#include <asio.hpp>
 
-#include "asio.hpp"
+using asio::ip::udp;
 
-// An asynchronous tcp echo server.
-// See https://think-async.com/Asio/asio-1.16.1/src/examples/cpp11/echo/async_tcp_echo_server.cpp
+enum { max_length = 1024 };
 
-using asio::ip::tcp;
-
-class session : public std::enable_shared_from_this<session> {
-public:
-  session(tcp::socket socket) : socket_(std::move(socket)) {}
-
-  void start() { do_read(); }
-
-private:
-  void do_read() {
-    auto self(shared_from_this());
-    socket_.async_read_some(asio::buffer(data_, max_length),
-                            [this, self](std::error_code ec, std::size_t length) {
-                              if (!ec) {
-                                do_write(length);
-                              }
-                            });
-  }
-
-  void do_write(std::size_t length) {
-    auto self(shared_from_this());
-    asio::async_write(socket_, asio::buffer(data_, length),
-                      [this, self](std::error_code ec, std::size_t /*length*/) {
-                        if (!ec) {
-                          do_read();
-                        }
-                      });
-  }
-
-  tcp::socket socket_;
-  enum { max_length = 1024 };
-  char data_[max_length];
-};
-
-class server {
-public:
-  server(asio::io_context& io_context, short port)
-      : acceptor_(io_context, tcp::endpoint(tcp::v4(), port)) {
-    do_accept();
-  }
-
-private:
-  void do_accept() {
-    acceptor_.async_accept([this](std::error_code ec, tcp::socket socket) {
-      if (!ec) {
-        std::make_shared<session>(std::move(socket))->start();
-      }
-
-      do_accept();
-    });
-  }
-
-  tcp::acceptor acceptor_;
-};
-
-int main(int argc, char* argv[]) {
-  try {
-    if (argc != 2) {
-      std::cerr << "Usage: async_tcp_echo_server <port>\n";
+int main(int argc, char* argv[])
+{
+  try
+  {
+    if (argc != 3)
+    {
+      std::cerr << "Usage: blocking_udp_echo_client <host> <port>\n";
       return 1;
     }
 
     asio::io_context io_context;
 
-    server s(io_context, std::atoi(argv[1]));
+    udp::socket s(io_context, udp::endpoint(udp::v4(), 0));
 
-    io_context.run();
-  } catch (std::exception& e) {
+    udp::resolver resolver(io_context);
+    udp::resolver::results_type endpoints =
+      resolver.resolve(udp::v4(), argv[1], argv[2]);
+
+    std::cout << "Enter message: ";
+    char request[max_length];
+    std::cin.getline(request, max_length);
+    size_t request_length = std::strlen(request);
+    s.send_to(asio::buffer(request, request_length), *endpoints.begin());
+
+    char reply[max_length];
+    udp::endpoint sender_endpoint;
+    size_t reply_length = s.receive_from(
+        asio::buffer(reply, max_length), sender_endpoint);
+    std::cout << "Reply is: ";
+    std::cout.write(reply, reply_length);
+    std::cout << "\n";
+  }
+  catch (std::exception& e)
+  {
     std::cerr << "Exception: " << e.what() << "\n";
   }
 
--- server/main.cpp
+++ server/main.cpp
@@ -1,82 +1,69 @@
 //
-// async_tcp_echo_server.cpp
+// async_udp_echo_server.cpp
 // ~~~~~~~~~~~~~~~~~~~~~~~~~
 //
-// Copyright (c) 2003-2020 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+// Copyright (c) 2003-2021 Christopher M. Kohlhoff (chris at kohlhoff dot com)
 //
-// Distributed under the Boost Software License, Version 1.0.
-// (See http://www.boost.org/LICENSE_1_0.txt)
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
 #include <cstdlib>
 #include <iostream>
-#include <memory>
-#include <utility>
+#include <asio.hpp>
 
-#include "asio.hpp"
+using asio::ip::udp;
 
-// An asynchronous tcp echo server.
-// See https://think-async.com/Asio/asio-1.16.1/src/examples/cpp11/echo/async_tcp_echo_server.cpp
-
-using asio::ip::tcp;
-
-class session : public std::enable_shared_from_this<session> {
+class server
+{
 public:
-  session(tcp::socket socket) : socket_(std::move(socket)) {}
+  server(asio::io_context& io_context, short port)
+    : socket_(io_context, udp::endpoint(udp::v4(), port))
+  {
+    do_receive();
+  }
 
-  void start() { do_read(); }
+  void do_receive()
+  {
+    socket_.async_receive_from(
+        asio::buffer(data_, max_length), sender_endpoint_,
+        [this](std::error_code ec, std::size_t bytes_recvd)
+        {
+          if (!ec && bytes_recvd > 0)
+          {
+            do_send(bytes_recvd);
+          }
+          else
+          {
+            do_receive();
+          }
+        });
+  }
+
+  void do_send(std::size_t length)
+  {
+    socket_.async_send_to(
+        asio::buffer(data_, length), sender_endpoint_,
+        [this](std::error_code /*ec*/, std::size_t /*bytes_sent*/)
+        {
+          do_receive();
+        });
+  }
 
 private:
-  void do_read() {
-    auto self(shared_from_this());
-    socket_.async_read_some(asio::buffer(data_, max_length),
-                            [this, self](std::error_code ec, std::size_t length) {
-                              if (!ec) {
-                                do_write(length);
-                              }
-                            });
-  }
-
-  void do_write(std::size_t length) {
-    auto self(shared_from_this());
-    asio::async_write(socket_, asio::buffer(data_, length),
-                      [this, self](std::error_code ec, std::size_t /*length*/) {
-                        if (!ec) {
-                          do_read();
-                        }
-                      });
-  }
-
-  tcp::socket socket_;
+  udp::socket socket_;
+  udp::endpoint sender_endpoint_;
   enum { max_length = 1024 };
   char data_[max_length];
 };
 
-class server {
-public:
-  server(asio::io_context& io_context, short port)
-      : acceptor_(io_context, tcp::endpoint(tcp::v4(), port)) {
-    do_accept();
-  }
-
-private:
-  void do_accept() {
-    acceptor_.async_accept([this](std::error_code ec, tcp::socket socket) {
-      if (!ec) {
-        std::make_shared<session>(std::move(socket))->start();
-      }
-
-      do_accept();
-    });
-  }
-
-  tcp::acceptor acceptor_;
-};
-
-int main(int argc, char* argv[]) {
-  try {
-    if (argc != 2) {
-      std::cerr << "Usage: async_tcp_echo_server <port>\n";
+int main(int argc, char* argv[])
+{
+  try
+  {
+    if (argc != 2)
+    {
+      std::cerr << "Usage: async_udp_echo_server <port>\n";
       return 1;
     }
 
@@ -85,7 +72,9 @@ int main(int argc, char* argv[]) {
     server s(io_context, std::atoi(argv[1]));
 
     io_context.run();
-  } catch (std::exception& e) {
+  }
+  catch (std::exception& e)
+  {
     std::cerr << "Exception: " << e.what() << "\n";
   }
 
