c4cd908c8c613c40aada044faa62fe6c3ed48ab5
Hjalte Sorgenfrei Mac Dalland <hjaltesorgenfrei@gmail.com>
2023-04-02 22:52:22 +0200

    Server keeps connections saved.

--- server/main.cpp
+++ server/main.cpp
@@ -15,6 +15,12 @@
 
 using asio::ip::udp;
 
+struct Connection
+{
+  std::shared_ptr<udp::endpoint> endpoint;
+  uint64_t guid;
+};
+
 class server
 {
 public:
@@ -26,20 +32,21 @@ public:
 
   void do_receive()
   {
+    auto senderEndpoint = std::make_shared<udp::endpoint>();
     _socket.async_receive_from(
-        asio::buffer(_recieveData, max_length), _senderEndpoint,
-        [this](std::error_code ec, std::size_t bytes_recvd)
+        asio::buffer(_recieveData, max_length), *senderEndpoint,
+        [this, senderEndpoint](std::error_code ec, std::size_t bytes_recvd)
         {
           if (!ec && bytes_recvd > 0)
           {
             Packet* packet = (Packet *)_recieveData;
-            handle_packet(packet);
+            handle_packet(packet, senderEndpoint);
           }
           do_receive();
         });
   }
 
-  void handle_packet(Packet *packet)
+  void handle_packet(Packet *packet, std::shared_ptr<udp::endpoint> senderEndpoint)
   {
     std::cout << "Received packet: " << packet_type_string(packet->type) << std::endl;
     switch (packet->type)
@@ -52,18 +59,33 @@ public:
         PacketConnectionAccepted response;
         response.client_guid = request->client_guid;
         response.connect_sequence = request->connect_sequence + 1;
-        do_send(sizeof(PacketConnectionAccepted), response);
+        
+        // If the guid is already in the list, remove it
+        for (auto it = _connections.begin(); it != _connections.end(); ++it)
+        {
+          if (it->guid == request->client_guid)
+          {
+            _connections.erase(it);
+            break;
+          }
+        }
+        Connection connection;
+        connection.endpoint = senderEndpoint;
+        connection.guid = request->client_guid;
+        _connections.push_back(connection);
+        do_send(sizeof(PacketConnectionAccepted), response, connection);
+        
         break;
       }
     }
   }
 
-  void do_send(std::size_t length, Packet &packet)
+  void do_send(std::size_t length, Packet &packet, Connection& connection)
   {
     std::shared_ptr<std::vector<char>> data = std::make_shared<std::vector<char>>(length);
     memcpy(data->data(), &packet, length);
     _socket.async_send_to(
-        asio::buffer(data->data(), length), _senderEndpoint,
+        asio::buffer(data->data(), length), *connection.endpoint,
         [data](std::error_code /*ec*/, std::size_t /*bytes_sent*/)
         {
         });
@@ -71,7 +93,7 @@ public:
 
 private:
   udp::socket _socket;
-  udp::endpoint _senderEndpoint;
+  std::vector<Connection> _connections;
   enum
   {
     max_length = 1024
@@ -84,15 +106,18 @@ int main(int argc, char *argv[])
 {
   try
   {
+    const char * port = "5000";
     if (argc != 2)
     {
       std::cerr << "Usage: async_udp_echo_server <port>\n";
-      return 1;
+    }
+    else {
+      port = argv[1];
     }
 
     asio::io_context io_context;
 
-    server s(io_context, std::atoi(argv[1]));
+    server s(io_context, std::atoi(port));
 
     io_context.run();
   }
